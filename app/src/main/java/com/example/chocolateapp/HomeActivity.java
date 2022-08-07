22f4dd4d1f0f156acd8467ecb2b96855393eea7f
Micoder-dev <micoder.com@gmail.com>
2022-08-07 21:13:09 +0530

    custom actionBar added with custom features like transparent statusBar, navigation

--- app/src/main/java/com/example/chocolateapp/HomeActivity.java
+++ app/src/main/java/com/example/chocolateapp/HomeActivity.java
@@ -1,14 +1,45 @@
 package com.example.chocolateapp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.shape.CornerFamily;
+import com.google.android.material.shape.MaterialShapeDrawable;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private MaterialToolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mToolBar = (MaterialToolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolBar);
+
+        float radius = getResources().getDimension(com.shashank.sony.fancywalkthroughlib.R.dimen.activity_half_margin);
+        MaterialShapeDrawable toolbarBackground = (MaterialShapeDrawable) mToolBar.getBackground();
+        toolbarBackground.setShapeAppearanceModel(
+                toolbarBackground.getShapeAppearanceModel()
+                        .toBuilder()
+                        .setBottomRightCorner(CornerFamily.ROUNDED,radius)
+                        .setBottomLeftCorner(CornerFamily.ROUNDED,radius)
+                        .build()
+        );
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        LayoutInflater layoutInflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View actionBarView = layoutInflater.inflate(R.layout.custom_app_bar, null);
+        actionBar.setCustomView(actionBarView);
+
     }
 }
